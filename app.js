91d27c0239999947785200afd1a1182b3dc5a19c
Yazan Deek <ydeek91@yahoo.com>
2020-09-29 17:56:26 -0400

    Updated app.js file.

--- app.js
+++ app.js
@@ -12,140 +12,195 @@ const render = require("./lib/htmlRenderer");
 
 
 
-let finalTeamArray = [];
+const employees = [];
 
-
-function startingPrompt() {
-    inquirer.prompt([
-        {
-            message: "Welcome to Team Generator 5000! Please write your team name:",
-            name: "teamname"
-        }
-    ])
-        .then(function (data) {
-            const teamName = data.teamname
-            finalTeamArray.push(teamName)
-            addManager();
-        })
-
-
+function initApp() {
+    startHtml();
+    addMember();
 }
 
-function addManager() {
-    inquirer.prompt([
-        {
-            message: "What is your team manager's name?",
-            name: "name"
-        },
-        {
-            message: "What is your team manager's email address?",
-            name: "email"
-        },
-
-        {
-            type: "number",
-            message: "What is your team manager's office number?",
-            name: "officeNumber"
-        },
-    ])
-
-        .then(function (data) {
-            const name = data.name
-            const id = 1
-            const email = data.email
-            const officeNumber = data.officeNumber
-            const teamMember = new Manager(name, id, email, officeNumber)
-            finalTeamArray.push(teamMember)
-            addTeamMembers();
-        });
-
-}
-
-function addTeamMembers() {
-    inquirer.prompt([
-        {
-            type: "list",
-            message: "Would you like to add more team members?",
-            choices: ["Yes, add an engineer", "Yes, add an intern", "No, my team is complete"],
-            name: "addMemberData"
-        }
-    ])
-
-        .then(function (data) {
-
-            switch (data.addMemberData) {
-                case "Yes, add an engineer":
-                    addEngineer();
-                    break;
-
-                case "Yes, add an intern":
-                    addIntern();
-                    break;
-                case "No, my team is complete":
-                    compileTeam();
-                    break;
+function addMember() {
+    inquirer.prompt([{
+        message: "Enter team member's name",
+        name: "name"
+    },
+    {
+        type: "list",
+        message: "Select team member's role",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Manager"
+        ],
+        name: "role"
+    },
+    {
+        message: "Enter team member's id",
+        name: "id"
+    },
+    {
+        message: "Enter team member's email address",
+        name: "email"
+    }])
+        .then(function ({ name, role, id, email }) {
+            let roleInfo = "";
+            if (role === "Engineer") {
+                roleInfo = "GitHub username";
+            } else if (role === "Intern") {
+                roleInfo = "school name";
+            } else {
+                roleInfo = "office phone number";
             }
+            inquirer.prompt([{
+                message: `Enter team member's ${roleInfo}`,
+                name: "roleInfo"
+            },
+            {
+                type: "list",
+                message: "Would you like to add more team members?",
+                choices: [
+                    "yes",
+                    "no"
+                ],
+                name: "moreMembers"
+            }])
+                .then(function ({ roleInfo, moreMembers }) {
+                    let newMember;
+                    if (role === "Engineer") {
+                        newMember = new Engineer(name, id, email, roleInfo);
+                    } else if (role === "Intern") {
+                        newMember = new Intern(name, id, email, roleInfo);
+                    } else {
+                        newMember = new Manager(name, id, email, roleInfo);
+                    }
+                    employees.push(newMember);
+                    addHtml(newMember)
+                        .then(function () {
+                            if (moreMembers === "yes") {
+                                addMember();
+                            } else {
+                                finishHtml();
+                            }
+                        });
+
+                });
         });
 }
 
-function addEngineer() {
-    inquirer.prompt([
-        {
-            message: "What is this engineer's name?",
-            name: "name"
-        },
-        {
-            message: "What is this engineer's email address?",
-            name: "email"
-        },
-        {
-            message: "What is this engineer's Github profile?",
-            name: "github"
+// function renderHtml(memberArray) {
+//     startHtml();
+//     for (const member of memberArray) {
+//         addHtml(member);
+//     }
+//     finishHtml();
+// }
+
+function startHtml() {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-dark mb-5">
+            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
+        </nav>
+        <div class="container">
+            <div class="row">`;
+    fs.writeFile("./output/team.html", html, function (err) {
+        if (err) {
+            console.log(err);
         }
-    ])
+    });
+    console.log("start");
+}
 
-        .then(function (data) {
-            const name = data.name
-            const id = finalTeamArray.length + 1
-            const email = data.email
-            const github = data.github
-            const teamMember = new Engineer(name, id, email, github)
-            finalTeamArray.push(teamMember)
-            addTeamMembers()
-        });
-
-};
-
-function addIntern() {
-    inquirer.prompt([
-        {
-            message: "What is this intern's name?",
-            name: "name"
-        },
-        {
-            message: "What is this intern's email address?",
-            name: "email"
-        },
-        {
-            message: "What is this intern's school?",
-            name: "school"
+function addHtml(member) {
+    return new Promise(function (resolve, reject) {
+        const name = member.getName();
+        const role = member.getRole();
+        const id = member.getId();
+        const email = member.getEmail();
+        let data = "";
+        if (role === "Engineer") {
+            const gitHub = member.getGithub();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Engineer</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">GitHub: ${gitHub}</li>
+            </ul>
+            </div>
+        </div>`;
+        } else if (role === "Intern") {
+            const school = member.getSchool();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Intern</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">School: ${school}</li>
+            </ul>
+            </div>
+        </div>`;
+        } else {
+            const officePhone = member.getOfficeNumber();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Manager</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">Office Phone: ${officePhone}</li>
+            </ul>
+            </div>
+        </div>`
         }
-    ])
-
-        .then(function (data) {
-            const name = data.name
-            const id = finalTeamArray.length + 1
-            const email = data.email
-            const school = data.school
-            const teamMember = new Intern(name, id, email, school)
-            finalTeamArray.push(teamMember)
-            addTeamMembers()
+        console.log("adding team member");
+        fs.appendFile("./output/team.html", data, function (err) {
+            if (err) {
+                return reject(err);
+            };
+            return resolve();
         });
-
-};
-
+    });
 
 
+
+
+
+
+}
+
+function finishHtml() {
+    const html = ` </div>
+    </div>
+    
+</body>
+</html>`;
+
+    fs.appendFile("./output/team.html", html, function (err) {
+        if (err) {
+            console.log(err);
+        };
+    });
+    console.log("end");
+}
+
+// addMember();
+// startHtml();
+// addHtml("hi")
+// .then(function() {
+// finishHtml();
+// });
+initApp();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
